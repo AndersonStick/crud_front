93bbe1e64f0e142fab940e537e4658d3fb89dc65
Anderson Barrera <andersonbar10@gmail.com>
2023-11-04 23:31:04 -0500

    aztualizacion paquetes

---
+++ src/app/producto/lista-producto.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { Producto } from '../models/producto';
+import { ProductoService } from '../service/producto.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-lista-producto',
+  templateUrl: './lista-producto.component.html',
+  styleUrls: ['./lista-producto.component.css']
+})
+export class ListaProductoComponent {
+
+  productos: Producto[] = [];
+
+  constructor(private productoService: ProductoService, 
+    private toastr: ToastrService
+    ) { }
+
+  ngOnInit() {
+    this.cargarProductos();
+  }
+
+  cargarProductos(): void {
+    this.productoService.lista().subscribe(
+      {
+        next: data => {
+          this.productos = data;
+        },
+        error: err => {
+          console.log(err);
+        }
+      }
+    );
+  }
+
+  borrar(id: number | undefined){
+    if (id !== undefined) {
+      this.productoService.delete(id).subscribe(
+        {
+          next: data => {
+            this.toastr.success('Producto Eliminado', 'OK', {
+              timeOut: 3000, positionClass: 'toast-top-center'
+              });
+              this.cargarProductos();
+          },
+          error: err => {
+            this.toastr.error(err.error.mensaje, 'Fail', {
+              timeOut: 3000, positionClass: 'toast-top-center',
+            });
+          }
+        }
+        );
+     }
+  }
+
+}
---
+++ src/app/producto/nuevo-producto.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ProductoService } from '../service/producto.service';
+import { Producto } from '../models/producto';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nuevo-producto',
+  templateUrl: './nuevo-producto.component.html',
+  styleUrls: ['./nuevo-producto.component.css']
+})
+export class NuevoProductoComponent {
+
+  nombre: string = '';
+  precio: number = 0;
+
+  constructor(
+    private productoService: ProductoService,
+    private toastr: ToastrService,
+    private router: Router
+    
+    ) { }
+
+  ngOnInit() {
+
+  }
+
+  onCreate(): void{
+    const producto = new Producto(this.nombre, this.precio);
+      this.productoService.save(producto).subscribe({
+        next: data => {
+          this.toastr.success('Producto creado', 'OK', {
+            timeOut: 3000, positionClass: 'toast-top-center'
+          });
+          this.router.navigate(['/']);
+        },
+        error: err => {
+          this.toastr.error(err.error.mensaje, 'Fail', {
+            timeOut: 3000, positionClass: 'toast-top-center',
+          });
+          this.router.navigate(['/']);
+        }
+      }
+    );
+  }
+
+}
